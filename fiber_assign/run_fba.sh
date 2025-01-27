81b579defd437dcaae1bfea024083874847072fa
biprateep <biprateep@pitt.edu>
2025-01-26 18:04:37 -0800

    update run_script

--- fiber_assign/run_fba.sh
+++ fiber_assign/run_fba.sh
@@ -3,7 +3,8 @@ source /global/common/software/desi/desi_environment.sh main
 module load desisurveyops
 export SKYHEALPIXS_DIR=/global/cfs/cdirs/desi/target/skyhealpixs/v1
 
+mv ./fba_output/*.yaml ./
 rm -r ./fba_output/*
-
+mv ./*.yaml ./fba_output
 
 python desi_fba_tertiary_9999.py --yamlfn tertiary-config-9999.yaml
